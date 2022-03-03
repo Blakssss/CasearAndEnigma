3600e5db9d1d1f6ced7ea8f7dcbc59c91b48ad71
Mathias <mathias.blak@gmail.com>
2022-03-03 15:15:36 +0100

    WAY better shit with ASCII and NUMBER program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,102 +1,122 @@
 package com.company;
-import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     Scanner input = new Scanner(System.in);
-    String alphabet1 = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String shift1Encrypt = "Z ABCDEFGHIJKLMNOPQRSTUVWXY";
-    String shift2Encrypt = "YZ ABCDEFGHIJKLMNOPQRSTUVWX";
-
-    char[] alphabet = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z'};
-    char[] shift1Decrypt = {'Z', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'};
-    char[] shift2Decrypt = {'Y', 'Z', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'};
     String encryptOrDecrypt;
     String stringInput;
+    int shiftKey;
+    int length;
+    static boolean exit = false;
+
     public static void main(String[] args) {
-        Main main = new Main();
-        main.enigmaType();
+        while (!exit) {
+            Main main = new Main();
+            main.enigmaType();
+        }
     }
 
-    public void enigmaType(){
+    public void enigmaType() {
         System.out.println("Welcome to the Enigma program. what type of encryption program would you like to run?\n1)Caesar?\n2)Vigenère?\n3)Numbers?\nType the number of your desired program. Or type 4)Exit to terminate the program:");
-        int number= input.nextInt();
+        int number = input.nextInt();
         input.nextLine(); //Scannerbug
         System.out.println("You chose nr." + number);
-        if (number == 1){
-            encryptOrDecryptCaesar();
+        if (number == 1) {
+            encryptOrDecrypt();
             encrypt();
             decrypt();
-            encryptOrDecryptCaesar();
-            encrypt();
-            decrypt();
-            terminate();
         }
-        if(number == 2){
+        if (number == 2) {
             System.out.println("We're sorry, the services you desired have not been implemented. Have a nice day.");
+            exit = true;
         }
-        if(number == 3){
-            System.out.println("We're sorry, the services you desired have not been implemented. Have a nice day.");
+        if (number == 3) {
+            encryptOrDecrypt();
+            encryptNumber();
+            decryptNumber();
         }
-        if (number == 4){
+        if (number == 4) {
             terminate();
         }
     }
 
-    public void encryptOrDecryptCaesar(){
+    public void encryptOrDecrypt() {
         System.out.println("\nWould you like to \"encrypt\" or \"decrypt\" a message?");
         encryptOrDecrypt = input.nextLine();
     }
+
     public void encrypt() {
         if (encryptOrDecrypt.equals("encrypt")) {
             System.out.println("What is your message?");
             stringInput = input.nextLine();
             stringInput = stringInput.toUpperCase();
-            int[] arrayOfInput = new int[stringInput.length()];
-
-            System.out.println("What key will you be using? 1 or 2?");
-            int shiftKey = input.nextInt();
-            input.nextLine(); //Scannerbug
-            if (shiftKey == 1) {
-                for (int i = 0; i < stringInput.length(); i++) {
-                    arrayOfInput[i] = stringInput.charAt(i);
-                    arrayOfInput[i] = shift1Encrypt.indexOf(arrayOfInput[i]);
-                    if(i == stringInput.length()-1){
-                        System.out.println(Arrays.toString(arrayOfInput));
-
-                    }
-                }
+            System.out.println("What shift-key will you be using?");
+            shiftKey = input.nextInt();
+            for (int i = 0; i < stringInput.length(); i++) {
+                int stringInputIndex = stringInput.charAt(i) + shiftKey;
+                if (stringInputIndex > 90) stringInputIndex = stringInputIndex - 26;
+                char encryptedMessage = (char) stringInputIndex;
+                System.out.print(encryptedMessage);
             }
-            if (shiftKey == 2) {
-                for (int i = 0; i < stringInput.length(); i++) {
-                    arrayOfInput[i] = stringInput.charAt(i);
-                    arrayOfInput[i] = shift2Encrypt.indexOf(arrayOfInput[i]);
-                    if(i == stringInput.length()-1){
-                        System.out.println(Arrays.toString(arrayOfInput));
-                    }
-                }
-            }
+            System.out.println("\n");
         }
     }
-    public void  decrypt(){
+
+    public void decrypt() {
         if (encryptOrDecrypt.equals("decrypt")) {
-            System.out.println("You have chosen to decrypt a message. What key will you be using?");
-            int shiftKey = input.nextInt();
+            System.out.println("You have chosen to decrypt a message. What shift-key will you be using?");
+            shiftKey = input.nextInt();
             System.out.println("Type in the encrypted message:");
-            if (shiftKey == 1) {
-                for (int i = 0; i < stringInput.length(); i++) {
-                    int intInput = input.nextInt();
-                    System.out.println(shift1Decrypt[intInput]);
-                }
+            input.nextLine();//Scanner-bug
+            stringInput = input.nextLine();
+            stringInput = stringInput.toUpperCase();
+            for (int i = 0; i < stringInput.length(); i++) {
+                int stringInputIndex = stringInput.charAt(i) - shiftKey;
+                if (stringInputIndex > 90) stringInputIndex = stringInputIndex - 26;
+                char decryptedMessage = (char) stringInputIndex;
+                System.out.print(decryptedMessage);
             }
-            if (shiftKey == 2) {
-                for (int i = 0; i < stringInput.length(); i++) {
-                    int intInput = input.nextInt();
-                    System.out.println(shift2Decrypt[intInput]);
-                }
-            }
+            System.out.println("\n");
         }
     }
-    void terminate(){
-        System.out.println("You have now encrypted and decrypted a basic message. You're like a spy or something. If you chose to simply exit the program however you're not like a spy.");
+    // NUMBER PROGRAM
+    public void encryptNumber() {
+        if (encryptOrDecrypt.equals("encrypt")) {
+            System.out.println("What is your message?");
+            stringInput = input.nextLine();
+            stringInput = stringInput.toUpperCase();
+            System.out.println("What shift-key will you be using?");
+            shiftKey = input.nextInt();
+            for (int i = 0; i < stringInput.length(); i++) {
+                int stringInputIndex = stringInput.charAt(i) + shiftKey;
+                if (stringInputIndex > 90) stringInputIndex = stringInputIndex - 26;
+
+                System.out.print(stringInputIndex + ",");
+            }
+            System.out.println("\n");
+        }
+    }
+    public void decryptNumber() {
+        if (encryptOrDecrypt.equals("decrypt")) {
+            System.out.println("You have chosen to decrypt a message. What shift-key will you be using?");
+            shiftKey = input.nextInt();
+            System.out.println("How long is your message?");
+            length = input.nextInt();
+            System.out.println("Type in the encrypted number:");
+            for (int i = 0; i < length; i++) {
+                int encryptedNumber = input.nextInt();
+                encryptedNumber = encryptedNumber - shiftKey;
+                char decryptedMessage = (char) encryptedNumber;
+                System.out.println(decryptedMessage);
+            }
+            System.out.println("\n");
+        }
+    }
+        // NUMBER PROGRAM
+
+        void terminate () {
+            System.out.println("You have now encrypted and decrypted a basic message. You're like a spy or something. \nIf you chose to simply exit the program however you're not like a spy.");
+            exit = true;
     }
 }
+
+
